ed4d6d8cb85ffbdb8ec07f097d8c679ab3ab2962
HanNabu <uniondirfolder@gmail.com>
2021-01-03 13:07:24 +0200

    Закончен хост Добавлен клиент (при добавлении ссылки на службу в клиенте хост должен работать)

--- chat_host/Program.cs
+++ chat_host/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,12 @@ namespace chat_host
     {
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(chat_cl.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Host starting!");
+                Console.ReadLine();
+            }
         }
     }
 }
